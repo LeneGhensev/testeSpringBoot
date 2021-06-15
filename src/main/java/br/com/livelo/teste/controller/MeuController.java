9718ab2cdca959808d48edcb324b77bd3a371da8
U-LIVELO\9000504 <9000504@LIVELODQMC03-N.livelo.elopar.corp>
2021-06-15 15:06:52 -0300

    Adiciona salva e consulta

--- src/main/java/br/com/livelo/teste/controller/MeuController.java
+++ src/main/java/br/com/livelo/teste/controller/MeuController.java
@@ -5,7 +5,10 @@ import br.com.livelo.teste.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class MeuController {
@@ -24,7 +27,20 @@ public class MeuController {
             System.out.println(u.getNome());
         }
         model.addAttribute("usuarios", usuarios);
-
         return "consulta";
+    }
+
+    @GetMapping("/adiciona")
+    public String adicionaUsuario(Usuario usuario, BindingResult result, Model model){
+        return "adiciona";
+    }
+
+    @RequestMapping("/salva")
+    public String salvaUsuarios(Usuario usuario, BindingResult result, Model model){
+        //Usuario usuario = new Usuario();
+        //usuario.setNome("Zé");
+        //usuario.setSenha("543");
+        usuarioService.save(usuario);
+        return "salva";
     }
 }
--- src/main/java/br/com/livelo/teste/repository/UsuarioRepositorio.java
+++ src/main/java/br/com/livelo/teste/repository/UsuarioRepositorio.java
@@ -4,5 +4,4 @@ import br.com.livelo.teste.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepositorio extends CrudRepository<Usuario, String>  {
-
 }
